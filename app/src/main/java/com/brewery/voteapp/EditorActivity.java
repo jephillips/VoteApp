58a394dabbded20025caae8bb01465c3c51bc733
Josh <jephillips@cohesionforce.com>
2015-03-10 15:00:49 -0500

    Created button that adds option fields to the poll creator

--- app/src/main/java/com/brewery/voteapp/EditorActivity.java
+++ app/src/main/java/com/brewery/voteapp/EditorActivity.java
@@ -1,20 +1,57 @@
+
+
 package com.brewery.voteapp;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Josh on 3/9/2015.
  */
 public class EditorActivity extends Activity {
 
+
+    private LinearLayout main;
+    private int id = 0;
+    private List<EditText> editTexts = new ArrayList<EditText>();
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.poll_editor);
+
+        main = (LinearLayout) findViewById(R.id.pollEditorLayout);
+
+        Button addButton = (Button) findViewById(R.id.addOptionButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                addVoteOption();
+            }
+        });
+
     }
+
+    private void addVoteOption() {
+        LinearLayout editTextLayout = new LinearLayout(this);
+        editTextLayout.setOrientation(LinearLayout.VERTICAL);
+        main.addView(editTextLayout);
+
+        EditText editText1 = new EditText(this);
+        editText1.setId(id++);
+        editTextLayout.addView(editText1);
+
+    }
+
 
     //This will eventually return a Poll
     public void saveAndReturn(View view) {
@@ -22,3 +59,4 @@ public class EditorActivity extends Activity {
         startActivity(mainReturn);
     }
 }
+
