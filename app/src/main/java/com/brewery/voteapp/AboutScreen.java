5903badf533d3423a4df91b49ce96986318e184f
Josh <jephillips@cohesionforce.com>
2015-03-17 15:26:30 -0500

    Added content to Help and About pages

--- app/src/main/java/com/brewery/voteapp/AboutScreen.java
+++ app/src/main/java/com/brewery/voteapp/AboutScreen.java
@@ -12,6 +12,7 @@ public class AboutScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
     }
 
 }
--- app/src/main/java/com/brewery/voteapp/HelpScreen.java
+++ app/src/main/java/com/brewery/voteapp/HelpScreen.java
@@ -12,6 +12,7 @@ public class HelpScreen extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_layout);
     }
 
 }
